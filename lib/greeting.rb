0879a3a9762abada31defcc376488229f7e8bdfa
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-11-20 14:28:27 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,5 @@
-# code the #greeting method here!
+
+# defines a greeting method
+def greeting(name) #takes in an argument of a persons name
+  puts "Hello #{name}. It's nice to meet you." # greets the person whose name was passed in as an argument
+end
