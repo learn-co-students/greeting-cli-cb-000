d3517b26c429a248f825cb5e8f6c82ab3bc6b914
chandu11288@gmail.com <chandu11288@gmail.com>
2018-01-12 23:26:21 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,6 @@
 # code the #greeting method here!
-puts "Hi! I'm HAL, what's your name?"
-name = gets.strip
+
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
+  print "Make sure bin/greet has code that can output 'Hi! I'm HAL, what's your name?' exactly."
 end
-
-greeting(name)
